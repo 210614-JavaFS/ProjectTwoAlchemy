cd588c1346810373c3999688cb3a12d8e69ada16
Natte Setobol <nsetobol@yahoo.com>
2021-08-10 19:04:46 -0700

    Made the user login return json/change the url mapping to /doLogin

--- project-two-back/src/main/java/com/revature/controllers/UserController.java
+++ project-two-back/src/main/java/com/revature/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ import com.revature.models.User;
 import com.revature.services.UserServices;
 
 @RestController
-@RequestMapping(value="/user")
+@RequestMapping(value="/user", consumes = {MediaType.APPLICATION_JSON_VALUE})
 @CrossOrigin
 public class UserController {
 	private UserServices userService;
@@ -31,24 +32,29 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public  ResponseEntity<String> userLogin(@RequestParam("username")  String username, @RequestParam("password") String password)
+	public  ResponseEntity<Object> userLogin(@RequestBody User loginUserInfo)
 	{
-		User user = userService.findUserByName(username);
+		// NOTES(): I don't know if this a good idea security wise, but it's much easier.
+		boolean success = false;
+		HashMap<String,String> returnedData = new HashMap<String,String>();
+		User user = userService.findUserByName(loginUserInfo.getUsername());
 		
 		if (user != null)
 		{
-			if (user.getPassword().equals(password))
+			if (user.getPassword().equals(loginUserInfo.getPassword()))
 			{
-				HashMap<String,String> test = new HashMap<String,String>();
-				test.put("username", username);
-				return ResponseEntity.ok(test.toString());
-			}
+				// TODO(): This should also return list of friends!
+				success = true;
+				returnedData.put("id", "" + user.getId());
+			} 
 		} 
 		
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
+		if (success == false)
+		{
+			returnedData.put("error", "Login is incorrect");
+		}
+		
+		return  new ResponseEntity<Object>(returnedData,HttpStatus.OK);
 	}
 	
-
-
 }
