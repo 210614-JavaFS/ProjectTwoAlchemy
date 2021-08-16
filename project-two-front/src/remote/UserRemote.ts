b83c42d2f4e2e1c55d5ac0a6afff00f8050e0dcb
JavierGabriel <javier.gonzalez35@upr.edu>
2021-08-16 03:35:57 -0400

    Revert "Update UserRemote.ts"
    This reverts commit 7759ffb3cd7a5e8ae07d2b9982e04e2e2d40917c.

--- project-two-front/src/remote/UserRemote.ts
+++ project-two-front/src/remote/UserRemote.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const UserRemote=axios.create({
-  baseURL:'http://18.222.23.181:8080/scrabbleapi/',
+  baseURL:'http://localhost:8080/project-two-back',
   headers:{
     'Content-Type':'application/json',
   }
