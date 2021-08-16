57d9df36d0f29eb730648d532685d3acbc228e2c
JavierGabriel <javier.gonzalez35@upr.edu>
2021-08-15 21:28:41 -0400

    register style

--- project-two-front/src/Component/register.tsx
+++ project-two-front/src/Component/register.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { User } from "../models/user";
 import { apiAddUser } from "../models/UserApi";
 import { fullUser } from '../utils/types';
@@ -30,27 +31,34 @@ function back(){
 
     return(
       <div className="container pt-5" >
-        <div className="row">
-         <form className="col s6" >
-             <div className="input-field col s6">
-                 <input  id="first_name" type="text" placeholder="First Name" className="validate"/>
-             </div>
-            <div className="input-field col s6">
-               <input id="last_name" type="text" placeholder="Last Name" className="validate"/>
-            </div>
-            <div className="row">
-              <div className="input-field col s6">
-                <input id="username" type="text" placeholder="Username" className="validate"/>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s6">
-                <input id="password" type="password" placeholder="Password" className="validate"/>
-              </div>
-            </div>
-            <button onClick={() => { sendInfo() }} className="btn" type="button">Submit</button>
-            <button  className="btn" type="submit" >cancel</button>
-        </form>
+        <div className="row justify-content-center">
+        <form className="col-lg-6">
+          <h3>Register</h3>
+          <div className="form-group">
+              <label>First name</label>
+              <input type="text" className="form-control" placeholder="First name" id="first_name" />
+          </div>
+
+          <div className="form-group">
+              <label>Last name</label>
+              <input type="text" className="form-control" placeholder="Last name" id="last_name"/>
+          </div>
+
+          <div className="form-group">
+              <label>Email</label>
+              <input type="text" className="form-control" placeholder="Enter username" id="username"/>
+          </div>
+
+          <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" placeholder="Enter password" id="password"/>
+          </div>
+
+          <button type="button" className="btn btn-dark btn-lg btn-block mt-1" onClick={() => { sendInfo() }}>Register</button>
+          <p className="forgot-password text-right">
+              Already registered? <Link to="/">Log In</Link>
+          </p>
+      </form>
         </div>
         </div>
     )
